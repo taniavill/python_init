a8d847663f9932cf18312ef5a96d5d6f99edf225
taniavill <tvillanueva@despegar.com>
2020-05-13 10:16:30 -0300

    mean and repeat errased

--- largest_repeat/repeat.py
+++ largest_repeat/repeat.py
@@ -1,6 +1,2 @@
 def largest_repeat(numbers):
-    largest = 0
-    for numb in numbers:
-        if numbers.count(numb) == numb:
-            largest = numb if numb > largest else largest
-    return largest
+    pass
--- mean_distances/dist.py
+++ mean_distances/dist.py
@@ -4,12 +4,4 @@ def mean_distance(p1, p2, p3):
     return math.dist(p1,p3)
 
 def mean_distance_t(p1, p2, p3):
-    first_d = math.dist(p1,p2)
-    second_d = math.dist(p2,p3)
-    third_d = math.dist(p3,p1)
-    print( first_d,second_d,third_d)
-    return (first_d+second_d+third_d)/3
-
-
-    #""" Given 3 points or (x, y) pairs, obtain the mean distance between them"""
-    #pass
+    pass
